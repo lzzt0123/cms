50cb1513d30063f3c356e665348d66656e79bf33
lzzt <1723194885@qq.com>
2023-01-28 22:04:50 +0800

    cms

---
+++ src/api/article-detail/services/article-detail.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * article-detail service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::article-detail.article-detail');
---
+++ src/api/article-type-tab/services/article-type-tab.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * article-type-tab service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::article-type-tab.article-type-tab');
